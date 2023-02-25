3e7d7073f3096f33020b5c43531e47c35056e857
Jin Seong Choe <jinseong@Jinseongui-MacBookAir.local>
2023-02-25 17:51:03 +0900

    2023/02/25

---
+++ study/BackJoon/C++17/1008.cpp
@@ -0,0 +1,15 @@
+// 2023.02.25 jinseong
+// A/B 출력
+    #include <iostream>
+    using namespace std;
+
+    int main(){
+        double a,b ;122
+        double c;
+        cin >> a >> b;
+        c = a/b;
+        cout.precision(10);
+        cout << c;
+
+        return 0;
+    }
