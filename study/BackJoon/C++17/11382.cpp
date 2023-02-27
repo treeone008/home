2c5bf41e2f8433aafbdf76d9e56af94853795b75
Jin Seong Choe <jinseong@Jinseongui-MacBookAir.local>
2023-02-27 10:21:33 +0900

    2023/02/27

---
+++ study/BackJoon/C++17/11382.cpp
@@ -0,0 +1,13 @@
+// 2023.02.27 jinseong
+// 세 수 더하기
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long a,b,c; // int형 초과범위
+    cin >> a >> b >> c;
+    cout << (a + b + c);
+
+    return 0;
+}
---
+++ study/BackJoon/Python3/10171.py
@@ -0,0 +1,8 @@
+# 2023.02.27 jinseong
+# 고양이모양 출력
+import sys
+
+print('\\    /\\')
+print(" )  ( ')")
+print('(  /  )')
+print(" \(__)|")
