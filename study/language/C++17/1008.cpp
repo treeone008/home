600f85021ee224d86fa1564f7a20f9f63083f7f4
Jin Seong Choe <jinseong@Jinseongui-MacBookAir.local>
2023-02-28 12:24:36 +0900

    2023/02/25

---
+++ study/language/C++17/1008.cpp
@@ -0,0 +1,16 @@
+// 2023.02.25 jinseong
+// A/B 출력
+// 1-5
+#include <iostream>
+using namespace std;
+
+int main(){
+    double a,b ;
+    double c;
+    cin >> a >> b;
+    c = a/b;
+    cout.precision(10);
+    cout << c;
+
+    return 0;
+}
