a67ce938cb9d688ee9498611e4d96de1b0ddd26b
Jin Seong Choe <jinseong@Jinseongui-MacBookAir.local>
2023-02-28 12:23:23 +0900

    2023/02/24

---
+++ study/language/C++17/1000.cpp
@@ -0,0 +1,13 @@
+// 2023.02.24 jinseong
+// 두 입력값의 합 출력
+// 1-2
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cin >> a >> b;
+    c = a+b;
+    cout << a+b;
+    return 0;
+}
