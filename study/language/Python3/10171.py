83045f2107e2a9da0cdf71c9a9522d0be2cb119e
treeone008 <109339847+treeone008@users.noreply.github.com>
2023-02-28 12:00:25 +0900

    Delete study/language directory

--- study/language/Python3/10171.py
+++
@@ -1,9 +0,0 @@
-# 2023.02.27 jinseong
-# 고양이모양 출력
-# 1-12
-import sys
-
-print('\\    /\\')
-print(" )  ( ')")
-print('(  /  )')
-print(" \(__)|")
--- study/language/Python3/10430.py
+++
@@ -1,8 +0,0 @@
-# 2023.02.26 jinseong
-# 나머지 값 계산
-# 1-9
-a,b,c = map(int,input().split())
-print((a+b)%c)
-print(((a%c)+(b%c))%c)
-print((a*b)%c)
-print(((a%c)*(b%c))%c)
